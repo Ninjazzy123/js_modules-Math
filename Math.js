3e48d14642e492c347ec9d54c6002626a3510520
Kevin Le <kevle@iu.edu>
2024-01-17 21:15:27 +0000

    first commit

---
+++ Math.js
@@ -0,0 +1,9 @@
+// Internal helper function that is not exported
+
+const addition = (x,y) => x+y;
+
+exports.calculate = function () {
+if (mathod == "addition") {
+    return addition(x,y);
+}
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+// //import module
+
+// const msg = require("./SimpleMessage.js")
+
+// //this will work
+// console.log(msg.good_msg)
+
+// //this won't work
+// console.log(msg.bad_msg)
+
+//bring in the calculate function as math
+const math = require("./Math")
+
+console.log(math.calculate("addition", 4, 7))
